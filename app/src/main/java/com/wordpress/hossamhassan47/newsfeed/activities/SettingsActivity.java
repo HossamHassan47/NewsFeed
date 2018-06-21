28e084946557953c33243cc46af1376cf9d0edeb
HossamHassan47 <hossam.hassan@outlook.com>
2018-06-21 15:26:46 +0200

    Setting Activity

--- app/src/main/java/com/wordpress/hossamhassan47/newsfeed/activities/SettingsActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/newsfeed/activities/SettingsActivity.java
@@ -1,8 +1,12 @@
 package com.wordpress.hossamhassan47.newsfeed.activities;
 
-import android.preference.PreferenceActivity;
+import android.content.SharedPreferences;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,11 +23,42 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction().replace(R.id.flContent, new MainSettingsFragment()).commit();
     }
 
-    public static class MainSettingsFragment extends PreferenceFragment{
+    public static class MainSettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            //Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            //bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            // The code in this method takes care of updating the displayed preference summary after it has been changed
+            String stringValue = newValue.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value
+                preference.setSummary(index > 0 ? listPreference.getEntries()[index] : null);
+            } else if (preference instanceof EditTextPreference) {
+                preference.setSummary(stringValue);
+            }
+
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+
+            String preferenceString = preferences.getString(preference.getKey(), "");
+
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
