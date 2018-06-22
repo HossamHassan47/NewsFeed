6ceed558f6e6a7925e4a604e65525d633433f54d
HossamHassan47 <hossam.hassan@outlook.com>
2018-06-22 09:19:06 +0200

    Setting Activity

--- app/src/main/java/com/wordpress/hossamhassan47/newsfeed/activities/SettingsActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/newsfeed/activities/SettingsActivity.java
@@ -1,14 +1,19 @@
 package com.wordpress.hossamhassan47.newsfeed.activities;
 
 import android.content.SharedPreferences;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 import com.wordpress.hossamhassan47.newsfeed.R;
 
@@ -29,8 +34,21 @@ public class SettingsActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
 
-            //Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
-            //bindPreferenceSummaryToValue(minMagnitude);
+            bindPreferenceSummaryToValue(findPreference("key_full_name"));
+            bindPreferenceSummaryToValue(findPreference("key_email"));
+            bindPreferenceSummaryToValue(findPreference("key_sleep_timer"));
+            bindPreferenceSummaryToValue(findPreference("key_music_quality"));
+            bindPreferenceSummaryToValue(findPreference("key_notification_ringtone"));
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+
+            String preferenceString = preferences.getString(preference.getKey(), "");
+
+            onPreferenceChange(preference, preferenceString);
         }
 
         @Override
@@ -43,22 +61,29 @@ public class SettingsActivity extends AppCompatActivity {
                 int index = listPreference.findIndexOfValue(stringValue);
 
                 // Set the summary to reflect the new value
-                preference.setSummary(index > 0 ? listPreference.getEntries()[index] : null);
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
             } else if (preference instanceof EditTextPreference) {
                 preference.setSummary(stringValue);
+            } else if (preference instanceof RingtonePreference){
+                if(TextUtils.isEmpty(stringValue)){
+                    // No ringtone selected
+                    preference.setSummary("Silent");
+                }else{
+                    Ringtone ringtone = RingtoneManager.getRingtone(preference.getContext(),
+                            Uri.parse(stringValue));
+
+                    if(ringtone == null){
+                        preference.setSummary("Choose notification ringtone");
+                    }else{
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
             }
 
             return true;
         }
 
-        private void bindPreferenceSummaryToValue(Preference preference) {
-            preference.setOnPreferenceChangeListener(this);
 
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
-
-            String preferenceString = preferences.getString(preference.getKey(), "");
-
-            onPreferenceChange(preference, preferenceString);
-        }
     }
 }
